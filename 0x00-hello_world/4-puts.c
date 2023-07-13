feaa68c813fd2c91b394079e53ffd69618446096
ilyasouagag <ilyasouagag2011@gmail.com>
2023-07-13 10:51:13 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-int main(int ac, char **av)
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts ("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
