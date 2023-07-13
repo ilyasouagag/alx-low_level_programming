357b7f54bc4a3bb91a76b91bcd3ec14dc1fba270
ilyasouagag <ilyasouagag2011@gmail.com>
2023-07-13 12:51:03 -0700

    write

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - contains write
+ *
+ * Return: 1 in this task
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora
+			 Korpar, 2015-10-19\n", 60 - 1);
+	return (1);
+}
+
