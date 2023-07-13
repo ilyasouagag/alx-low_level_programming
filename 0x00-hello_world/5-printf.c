48477d97f88f34c0b062539e313c3ad0efa579be
ilyasouagag <ilyasouagag2011@gmail.com>
2023-07-13 11:14:54 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main: is the function that will be execute
+ *
+ * printf: returns the output
+ *
+ * return : return 0 in this case
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
+	return (0);
+}
