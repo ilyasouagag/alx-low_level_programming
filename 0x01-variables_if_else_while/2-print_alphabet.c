043b3861d59b0b2c1b9efccbd27db7e7058bdbb4
ilyasouagag <ilyasouagag2011@gmail.com>
2023-07-14 09:58:21 -0700

    putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - conatains the programm
+ *
+ * Return: 0 in main
+ */
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		putchar(alpha);
+		alpha++;
+	}
+	return (0);
+}
