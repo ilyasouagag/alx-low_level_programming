29ea50e69178db8d183bd49833b61c42337383fb
ilyasouagag <ilyasouagag2011@gmail.com>
2023-07-14 14:41:14 -0700

    4 nested loops

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - contains the code
+ *
+ * Return: 0 always in main
+ */
+int main(void)
+{
+	int i, j, k, m;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			for (k = 0; k <= 9; k++)
+			{
+				for (m = j + 1; m <= 9; m++)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(' ');
+					putchar(k + '0');
+					putchar(m + '0');
+					if (i == 9 && j == 8 && k == 8 && m == 9)
+					{
+						putchar('\n');
+						break;
+					}
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	return (0);
+}
