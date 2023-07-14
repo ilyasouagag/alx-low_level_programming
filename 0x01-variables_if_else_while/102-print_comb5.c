f2a077920be0cd659639aa9d6e7977c5305c44b1
ilyasouagag <ilyasouagag2011@gmail.com>
2023-07-14 16:03:30 -0700

    2 nested loops

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,29 +7,23 @@
  */
 int main(void)
 {
-	int i, j, k, m;
+	int i, j;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i <= 98; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (j = i + 1; j <= 99; j++)
 		{
-			for (k = i; k <= 9; k++)
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(' ');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+			if (i == 98 && j == 99)
 			{
-				for (m = j + 1; m <= 9; m++)
-				{
-					putchar(i + '0');
-					putchar(j + '0');
-					putchar(' ');
-					putchar(k + '0');
-					putchar(m + '0');
-					if (i == 9 && j == 8 && k == 8 && m == 9)
-					{
-						continue;
-					}
-					putchar(',');
-					putchar(' ');
-				}
+				break;
 			}
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
