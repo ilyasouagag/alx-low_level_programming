9953a08dc45b3e30518c98bcaa1ea89c1e5c487d
ilyasouagag <ilyasouagag2011@gmail.com>
2023-07-14 11:47:48 -0700

    a lot of putchar

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - contains the code
+ *
+ * Return: 0 always in main
+ */
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		putchar(',');
+		putchar(' ');
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
