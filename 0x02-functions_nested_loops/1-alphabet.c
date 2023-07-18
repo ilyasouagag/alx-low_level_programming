9f2b337b2868888cfb1674e6db12e0bdef11adc6
ilyasouagag <ilyasouagag2011@gmail.com>
2023-07-18 11:00:00 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: prints alphabet
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	void print_alphabet(void);
+	return (0);
+}
+
