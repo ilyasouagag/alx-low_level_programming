ecee9061fd2ad8d049beeda19c565ad80ab6824c
ilyasouagag <ilyasouagag2011@gmail.com>
2023-07-18 15:05:20 -0700

    print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - prints the last digit of a number
+ *
+ * @n: parameter of function
+ *
+ * Return: always 0 (Success)
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for ( ; n <= 97; n++)
+		{
+			printf("%d, ", n);
+		}
+	}
+	else
+	{
+		for ( ; n > 98; n--)
+		{
+			printf("%d, ", n);
+		}
+	}
+}
