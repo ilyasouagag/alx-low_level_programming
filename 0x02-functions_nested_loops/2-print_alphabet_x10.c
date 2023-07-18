2191ee32246a190c4a3fd780168aea5e2a111d59
ilyasouagag <ilyasouagag2011@gmail.com>
2023-07-18 11:46:32 -0700

    10 times alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10: print 10 times the alphabet
+ *
+ * Return: return the value
+ */
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int i = 0;
+
+	while (i < 10)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+		i++;
+	}
+}
