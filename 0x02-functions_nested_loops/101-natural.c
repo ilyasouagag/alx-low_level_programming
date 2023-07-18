99afda19d57cbd416ed7d75a019ab11f9197f42f
ilyasouagag <ilyasouagag2011@gmail.com>
2023-07-18 16:17:42 -0700

    sum bellow 1024

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * sum_natural - adds two integers and returns the result
+ *
+ * Return: always 0 (Success)
+ */
+int sum_natural(void)
+{
+	int i = 0, sum = 0;
+
+	while (i < 1024)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+		i++;
+	}
+	printf("%d", sum);
+}
