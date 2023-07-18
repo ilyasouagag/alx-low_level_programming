b7a94fc5b5c0d886c2c83ca825dd9a5133e81299
ilyasouagag <ilyasouagag2011@gmail.com>
2023-07-18 12:45:34 -0700

    valeur absolue

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - checks for lowercase character
+ *
+ * @int: parameter of function
+ *
+ * Return: always 0 (Success)
+ */
+int _abs(int)
+{
+	int a;
+
+	if (a < 0)
+		return (-a);
+	return (a);
+}
