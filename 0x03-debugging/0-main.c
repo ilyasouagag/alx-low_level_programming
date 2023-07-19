0f726dbdab1a3476510cde162154301490ae307c
ilyasouagag <ilyasouagag2011@gmail.com>
2023-07-19 12:14:03 -0700

    task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * positive_or_negative - function
+ *
+ * main - entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
