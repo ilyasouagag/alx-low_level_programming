f0b4bb9594e5a8d1947ebcfbec2ed244f133b953
ilyasouagag <ilyasouagag2011@gmail.com>
2023-07-20 16:57:39 -0700

    larget prime number

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -22,30 +22,27 @@ double square(double a)
 	return (sqrt);
 }
 /**
- * nb_premier - function that determine if a number is prime or not
+ * lar_prime_fac - function that determine if a number is prime or not
  *
- * @n: parameter
- *
- * Return: return if the number is prime or not
+ * @num: parameter
  */
-int nb_premier(long long int n)
+void lar_prime_fac(long int num)
 {
-	if (n <= 1)
+	int i, lar;
+
+	while (num % 2 == 0)
+		num = num / 2;
+	for (i = 3; i <= square(num); i += 2)
 	{
-		return (0);
-	}
-	if (n == 2)
-	{
-		return (1);
-	}
-	for (long long int i = 2; i <= square(n); i++)
-	{
-		if (n % i == 0)
+		while (num % i == 0)
 		{
-			return (0);
+			num = num / i;
+			lar = i;
 		}
-}
-	return (1);
+	}
+	if (num > 2)
+		lar = num;
+	printf("%d\n", lar);
 }
 /**
  * main - return the largest prime factor
@@ -55,28 +52,7 @@ int nb_premier(long long int n)
 
 int main(void)
 {
-	long long int num = 612852475143;
-	long long int larg_num_fac = 1;
+	lar_prime_fac(612852475143);
 
-	for (long long int i = 2; i <= sqrt(num); i++)
-	{
-		if (num % i == 0)
-		{
-			OA
-			if (nb_premier(i))
-			{
-				larg_num_fac = i;
-			}
-			while (num % i == 0)
-			{
-				num /= i;
-			}
-		}
-	}
-	if (num > 1)
-	{
-		larg_num_fac = num;
-	}
-	printf("The largest prime factor is %lld\n", larg_num_fac);
 	return (0);
 }
