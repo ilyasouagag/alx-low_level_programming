8ce2a24a92e524b9343c1769031a97b87d07b3db
ilyasouagag <ilyasouagag2011@gmail.com>
2023-07-20 08:46:47 -0700

    for loop

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that checks for uppercase character
+ *
+ * Return: return the value of the function
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar("\n");
+}
