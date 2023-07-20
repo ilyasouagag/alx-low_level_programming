3009765b9bfa6941aa66d84882aff2cd17856c0c
ilyasouagag <ilyasouagag2011@gmail.com>
2023-07-20 10:03:07 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * more_numbers - function that prints 10 times the numbers
+ * @n: parameter
+ */
+void print_line(int n);
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
