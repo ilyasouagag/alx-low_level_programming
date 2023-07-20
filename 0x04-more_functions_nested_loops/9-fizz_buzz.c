6a3c148832b61bfa85f6be39a4d6215482ddbfbe
ilyasouagag <ilyasouagag2011@gmail.com>
2023-07-20 11:19:35 -0700

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - function that darws a diagonal line on the terminal
+ *
+ * Return: always 0 in main
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d ", i);
+	}
+	return (0);
+}
