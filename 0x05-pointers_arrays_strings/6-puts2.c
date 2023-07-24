d334aa5c97f54c5d5732e9bfdba4eff6b0dcd968
ilyasouagag <ilyasouagag2011@gmail.com>
2023-07-24 12:56:29 -0700

    sauter un caractère

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,14 +6,13 @@
  */
 void puts2(char *str)
 {
-	while (*str)
+	int i = 0;
+
+	while (str[i] != 0)
 	{
-		_putchar(*str);
-		str += 2;
-		if (*str == '\0' && *(str - 1) != '\0')
-		{
-			break;
-		}
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
 	}
-	_putchar('\n');
+	putchar('\n');
 }
