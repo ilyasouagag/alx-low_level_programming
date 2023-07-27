802ce53f8054f70a653f9b1ff4013e1fec8ffcd7
ilyasouagag <ilyasouagag2011@gmail.com>
2023-07-27 15:39:03 -0700

    recursion

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -5,28 +5,17 @@
  */
 void print_number(int n)
 {
-	unsigned int i = 0;
-	int j = 0;
+	unsigned int m;
 
+	m = n;
 	if (n < 0)
 	{
-		_putchar('-');
-		n = -n;
+		putchar('-');
+		m = -n;
 	}
-	if (n == 0)
+	if (m / 10 != 0)
 	{
-		_putchar(0 + '0');
+		print_number(m / 10);
 	}
-	while (n > 0)
-	{
-		j = n % 10;
-		n = n / 10;
-		i = (i * 10) + j;
-	}
-	while (i > 0)
-	{
-		_putchar((i % 10) + '0');
-		
-		i = i / 10;
-	}
+	putchar((m % 10) + '0');
 }
