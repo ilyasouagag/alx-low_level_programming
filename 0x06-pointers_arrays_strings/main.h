81c77b6985bc27213ad80e091f3f5d4a8cc8ce20
ilyasouagag <ilyasouagag2011@gmail.com>
2023-07-27 17:03:38 -0700

    buffer

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -14,4 +14,7 @@ char *rot13(char *);
 int _putchar(char c);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+void print_line(char *c, int s, int l);
+void print_buffer(char *b, int size);
 #endif
