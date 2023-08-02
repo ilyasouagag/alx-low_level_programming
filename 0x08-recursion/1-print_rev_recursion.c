6019372506f30214568b95b5700f43c721ff5ddb
ilyasouagag <ilyasouagag2011@gmail.com>
2023-08-02 15:26:19 -0700

    reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,2 +1,13 @@
 #include "main.h"
-
+/**
+ * _print_rev_recursion - function that prints a string
+ * @s: pointer to string
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != 0)
+	{
+		_print_rev_recursion(s + 1);
+		putchar(*s);
+	}
+}
