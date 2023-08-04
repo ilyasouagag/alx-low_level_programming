6a1bee8e12b4c81b29b04a5f2876f06f07ee1dfc
ilyasouagag <ilyasouagag2011@gmail.com>
2023-08-04 14:30:23 -0700

    make change for an amount of money

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - change cents
+ * @argc: counts the number of arguments
+ * @argv: display arguments
+ * Return: return money changed
+ */
+int main(int argc, int *argv[])
+{
+	int array[5] = {25, 10, 5, 2, 1};
+	int i, cents = 0;
+
+	if (argc == 1 || argc > 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	if (argv[1] < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 0; i < 5 && atoi(argv[1]) >= 0; i++)
+	{
+		while (atoi(argv[1] >= array[i]))
+		{
+			cents++;
+			argv[1] -= array[i];
+		}
+	}
+	printf("%d\n", cents);
+	return (0);
+}
+
