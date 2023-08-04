6a45304af0653efa50b8d0c770498db169cc0233
ilyasouagag <ilyasouagag2011@gmail.com>
2023-08-04 11:36:59 -0700

    print the name of program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - executable function
+ * @argc: counts the number of arguments
+ * @argv: display content of each argument
+ * Return: return 0
+ */
+int main(int argc, char **argv)
+{
+	printf("%s", argv[0]);
+	return (0);
+}
