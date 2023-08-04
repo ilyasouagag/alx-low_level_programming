950c586859921bfc6e6a8d3d438368fe40e0fb6a
ilyasouagag <ilyasouagag2011@gmail.com>
2023-08-04 12:30:15 -0700

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -28,14 +28,16 @@ int _atoi(char *s)
  */
 int main(int argc, char **argv)
 {
-	int mul;
+	int mul, num1, num2;
 
 	if (argc < 3 || argc > 3)
 	{
 		printf("error\n");
 		return (1);
 	}
-	mul = _atoi(argv[1]) * _atoi(argv[2]);
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
+	result = num1 * num2;
 	printf("%d\n", mul);
 	return (0);
 }
