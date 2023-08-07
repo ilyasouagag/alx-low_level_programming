369110675b0be1a86d7e13de6a3f0e3319d886e7
ilyasouagag <ilyasouagag2011@gmail.com>
2023-08-07 15:19:56 -0700

    2 D array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * alloc_grid - function that returns a pointer to a 2 D array
+ * @width: width of matrix
+ * @height: height of matrix
+ * Return: return a matrix
+ */
+int **alloc_grid(int width, int height)
+{
+	int **p, i;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	*p = (int **) calloc(height, sizeof(int *));
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		p[i] = (int *) calloc(width, sizeof(int));
+		if (p[i] == NULL)
+			return (NULL);
+	}
+	return (p);
+}
