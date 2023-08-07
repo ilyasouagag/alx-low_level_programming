f211874b58b2e5a53ed4f74fba8a3ecb51777d0c
ilyasouagag <ilyasouagag2011@gmail.com>
2023-08-07 16:31:19 -0700

    free colums and then first pointer to lines

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -10,9 +10,8 @@ void free_grid(int **grid, int height)
 	int k;
 
 	if (grid == NULL || height == 0)
-		return (NULL);
+		return;
 	for (k = 0; k <= height; k++)
 		free(grid[k]);
 	free(grid);
-	return (NULL);
 }
