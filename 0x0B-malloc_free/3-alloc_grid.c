d0f3296134433dfc9448d68eade77123c8497249
ilyasouagag <ilyasouagag2011@gmail.com>
2023-08-07 16:12:56 -0700

    free all previous lines if one line = null

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,18 +11,25 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	p = (int **)calloc(height, sizeof(int *));
+	p = (int **)malloc(height * sizeof(int *));
 	if (p == NULL)
 		return (NULL);
 	for (i = 0; i < height; i++)
 	{
-		p[i] = (int *)calloc(width, sizeof(int));
+		p[i] = (int *)malloc(width * sizeof(int));
 		if (p[i] == NULL)
 		{
-			free(p);
 			for (k = 0; k <= i; k++)
 				free(p[k]);
+			free(p);
 			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			p[i][j] = 0;
 		}
 	}
 
