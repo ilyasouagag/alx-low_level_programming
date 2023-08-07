b9e589c44c19386536997fc395760fde4e45517a
ilyasouagag <ilyasouagag2011@gmail.com>
2023-08-07 13:35:54 -0700

    newly allocated space

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _strdup - function that returns a pointer to a newly space in memory
+ * @str: given string
+ * Return: return array
+ */
+
+char *_strdup(char *str)
+{
+	char *p = str;
+	int len;
+	int i = 0;
+	char *array;
+
+	if (str == NULL)
+		return (NULL);
+	while (*p)
+	{
+		len++;
+		p++;
+	}
+	array = (char *)malloc(sizeof(char) * len);
+	if (array == NULL)
+		printf("insufficient memory avalaible!");
+	return (NULL);
+	while (str[i] != 0)
+	{
+		array[i] = str[i];
+		i++;
+	}
+	array[len] = '\0';
+	return (array);
+}
