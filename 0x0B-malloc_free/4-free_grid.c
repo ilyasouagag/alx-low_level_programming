395aba0708483b8c951f0801347db73077456881
ilyasouagag <ilyasouagag2011@gmail.com>
2023-08-07 16:27:22 -0700

    free colums and then first pointer to lines

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * free_grid - function that returns a pointer to a 2 D array
+ * @grid: pointer to matrix
+ * @height: height of matrix
+ * Return: return null
+ */
+void free_grid(int **grid, int height)
+{
+	int k;
+
+	if (grid == NULL || height == 0)
+		return (NULL);
+	for (k = 0; k <= height; k++)
+		free(grid[k]);
+	free(grid);
+	return (NULL);
+}
