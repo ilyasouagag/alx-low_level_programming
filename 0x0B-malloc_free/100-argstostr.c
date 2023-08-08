b8e4e5c0a784ebfab7d400859d6dd7b3c5732868
ilyasouagag <ilyasouagag2011@gmail.com>
2023-08-08 15:03:41 -0700

    we create a pointer woth malloc that point to all characters

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -16,7 +16,7 @@ char *argstostr(int ac, char **av)
 	for (i = 0; i < ac; i++)
 	{
 		j = 0;
-		while (av[i][j] != NULL)
+		while (av[i][j] != 0)
 		{
 			len++;
 			j++;
@@ -30,7 +30,7 @@ char *argstostr(int ac, char **av)
 	for (i = 0; i < ac; i++)
 	{
 		j = 0;
-		while (av[i][j] != NULL)
+		while (av[i][j] != 0)
 		{
 			p[k] = av[i][j];
 			j++;
