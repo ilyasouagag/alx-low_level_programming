644faa1f8fc6eb4e6fafb2789b9e27478a2d7ca2
ilyasouagag <ilyasouagag2011@gmail.com>
2023-08-10 15:06:35 -0700

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,7 +12,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	str = (void *)malloc(size * nmemb);
+	str = malloc(size * nmemb);
 	if (str == NULL)
 		return (NULL);
 	for (i = 0; i < nmemb; i++)
