dc17c9d5e8cfb48212a39f4e8d7dad6fab22fa4b
ilyasouagag <ilyasouagag2011@gmail.com>
2023-08-10 14:10:13 -0700

    concatenates

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -8,14 +8,28 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len1, len2, i, j;
+	unsigned int len1 = 0, len2 = 0;
+	unsigned int i = 0, j = 0;
 	char *s3;
 
-	len1 = len2 = i = j = 0;
-	while (s1 && s1[len1])
-		len1++;
-	while (s2 && s2[len2])
-		len2++;
+	if (s1 == NULL)
+	{
+		len1 = 0;
+	}
+	else
+	{
+		while (s1 && [len1])
+			len1++;
+	}
+	if (s2 == NULL)
+	{
+		len2 = 0;
+	}
+	else
+	{
+		while (s2 && s2[len2])
+			len2++;
+	}
 	if (n > len2)
 		n = len2;
 	s3 = (char *)malloc(sizeof(char) * (len1 + n + 1));
@@ -26,12 +40,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s3[i] = s1[i];
 		i++;
 	}
-	while (s2[j] != 0 && j < n)
+	while (s2[j] != 0 && j < n && i < len1 + n)
 	{
 		s3[i] = s2[j];
 		j++;
 		i++;
 	}
-	s3[i] = '\0';
+	s3[len1 + n] = '\0';
 	return (s3);
 }
