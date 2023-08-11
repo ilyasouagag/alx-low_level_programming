77db8d236272fae9882f0bc7f20cfaec72374018
ilyasouagag <ilyasouagag2011@gmail.com>
2023-08-10 18:06:41 -0700

    multiplication

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -9,4 +9,9 @@ char *_memset(char *s, char b, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void _print(char *str, int l);
+char *mul(char n, char *num, int num_index, char *dest, int dest_index);
+int check_for_digits(char **av);
+void init(char *str, int l);
+int _putchar(char c);
 #endif
