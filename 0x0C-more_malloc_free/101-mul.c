fd46c3501ed362adf399ee26c786f48b65bc2ea8
ilyasouagag <ilyasouagag2011@gmail.com>
2023-08-10 18:03:43 -0700

    multiplication

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,85 +1,146 @@
 #include "main.h"
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * _print - moves a string one place to the left and prints the string
+ * @str: string to move
+ * @l: size of string
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: void
  */
-int _putchar(char c)
+void _print(char *str, int l)
 {
-	return (write(1, &c, 1));
-}
-/**
- * puts - print string
- * @ptr: string
- */
-void _puts(char *ptr)
-{
-	int i = 0;
+	int i, j;
 
-	while (ptr[i])
+	i = j = 0;
+	while (i < l)
 	{
-		_putchar(ptr[i]);
+		if (str[i] != '0')
+			j = 1;
+		if (j || i == l - 1)
+			_putchar(str[i]);
 		i++;
 	}
-}
-/**
- * print_int - prints integer
- * @n: number
- */
-void print_int(unsigned long int n)
-{
-	unsigned long int div = 1, i, res;
 
-	for (i = 0; n / div > 9; i++, div *= 10)
-		;
-	for (; div >= 1; n %= div, div /= 10)
-	{
-		res = n / div;
-		_putchar('0' + res);
-	}
-}
-/**
- * _atoi - convert a string to an integer
- *
- * @s: pointer
- * Return: returns the integer
- *
- * num will enter (number > 0) only if
- *he is a caracter so we will break the loop
- */
-int _atoi(char *s)
-{
-	unsigned int number = 0;
-	int t = 1;
-
-	do {
-		if (*s == '-')
-			t *= -1;
-		else if (*s >= '0' && *s <= '9')
-			number = (number * 10) + (*s - '0');
-		else if (number > 0)
-			break;
-	} while (*s++);
-	return (number * t);
-}
-/**
- * main - main function
- * @argc: number of arguments
- * @argv: arguments
- * Return: 0
- */
-int main(int argc, char *argv[])
-{
-	(void)argc;
-
-	if (argc != 3)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	print_int(_atoi(argv[1]) * _atoi(argv[2]));
 	_putchar('\n');
+	free(str);
+}
+
+/**
+ * mul - multiplies a char with a string and places the answer into dest
+ * @n: char to multiply
+ * @num: string to multiply
+ * @num_index: last non NULL index of num
+ * @dest: destination of multiplication
+ * @dest_index: highest index to start addition
+ *
+ * Return: pointer to dest, or NULL on failure
+ */
+char *mul(char n, char *num, int num_index, char *dest, int dest_index)
+{
+	int j, k, mul, mulrem, add, addrem;
+
+	mulrem = addrem = 0;
+	for (j = num_index, k = dest_index; j >= 0; j--, k--)
+	{
+		mul = (n - '0') * (num[j] - '0') + mulrem;
+		mulrem = mul / 10;
+		add = (dest[k] - '0') + (mul % 10) + addrem;
+		addrem = add / 10;
+		dest[k] = add % 10 + '0';
+	}
+	for (addrem += mulrem; k >= 0 && addrem; k--)
+	{
+		add = (dest[k] - '0') + addrem;
+		addrem = add / 10;
+		dest[k] = add % 10 + '0';
+	}
+	if (addrem)
+	{
+		return (NULL);
+	}
+	return (dest);
+}
+
+/**
+ * check_for_digits - checks the arguments to ensure they are digits
+ * @av: pointer to arguments
+ *
+ * Return: 0 if digits, 1 if not
+ */
+int check_for_digits(char **av)
+{
+	int i, j;
+
+	for (i = 1; i < 3; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			if (av[i][j] < '0' || av[i][j] > '9')
+				return (1);
+		}
+	}
 	return (0);
 }
+
+/**
+ * init - initializes a string
+ * @str: sting to initialize
+ * @l: length of strinf
+ *
+ * Return: void
+ */
+void init(char *str, int l)
+{
+	int i;
+
+	for (i = 0; i < l; i++)
+		str[i] = '0';
+	str[i] = '\0';
+}
+
+/**
+ * main - multiply two numbers
+ * @argc: number of arguments
+ * @argv: argument vector
+ *
+ * Return: zero, or exit status of 98 if failure
+ */
+
+int main(int argc, char *argv[])
+{
+	int l1, l2, ln, ti, i;
+	char *a;
+	char *t;
+	char e[] = "Error\n";
+
+	if (argc != 3 || check_for_digits(argv))
+	{
+		for (ti = 0; e[ti]; ti++)
+			_putchar(e[ti]);
+		exit(98);
+	}
+	for (l1 = 0; argv[1][l1]; l1++)
+		;
+	for (l2 = 0; argv[2][l2]; l2++)
+		;
+	ln = l1 + l2 + 1;
+	a = malloc(ln * sizeof(char));
+	if (a == NULL)
+	{
+		for (ti = 0; e[ti]; ti++)
+			_putchar(e[ti]);
+		exit(98);
+	}
+	init(a, ln - 1);
+	for (ti = l2 - 1, i = 0; ti >= 0; ti--, i++)
+	{
+		t = mul(argv[2][ti], argv[1], l1 - 1, a, (ln - 2) - i);
+		if (t == NULL)
+		{
+			for (ti = 0; e[ti]; ti++)
+				_putchar(e[ti]);
+			free(a);
+			exit(98);
+		}
+	}
+	_print(a, ln - 1);
+	return (0);
