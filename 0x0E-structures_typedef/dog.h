2a9ecc24d4032b2a33f4b62a16ad06f777a57370
ilyasouagag <ilyasouagag2011@gmail.com>
2023-08-14 06:55:23 -0700

    first commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#ifndef DOG_H
+#define DOD_H
+/**
+ * struct dog - structure
+ * @name: name of dog
+ * @age: his age
+ * @owner: the name of the owner
+ */
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+};
+#endif 
