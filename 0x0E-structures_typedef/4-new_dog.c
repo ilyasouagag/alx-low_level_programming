5f1d08a9c32664fccfd37abf114e0fbf6f9910c5
ilyasouagag <ilyasouagag2011@gmail.com>
2023-08-14 08:14:35 -0700

    malloc with struct

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,49 @@
+#include "dog.h"
+/**
+ * new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: his age
+ * @owner: owner of the dog
+ * Return: return the created struct
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int a = 0, b = 0, len1 = 0, len2 = 0;
+	dog_t *dog;
+
+	while (*name && name[a])
+		len1++;
+	while (*owner && owner[b])
+		len2++;
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+	dog->name = malloc(sizeof(char) * len1 + 1);
+	if (dog->name == NULL)
+	{
+
+		free(dog);
+		return (NULL);
+	}
+	dog->owner = malloc(sizeof(char) * len2 + 1);
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	int i, j;
+
+	dog->age = age;
+	for (i = 0; name[i] != 0; i++)
+	{
+		dog->name[i] = name[i];
+	}
+	dog->name[i] = '\0';
+	for (j = 0; owner[j] != 0; j++)
+	{
+		dog->owner[j] = ownner[j];
+	}
+	dog->owner[j] = '\0';
+	return (dog);
+}
