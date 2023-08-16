e9d5b85faebaa15bde5c6c6dce8b8f2c29f12720
ilyas <ilyasouagag2011@gmail.com>
2023-08-16 17:26:48 +0100

    not gonna work

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,34 @@
+#include "calc.h"
+/**
+ * main - main function
+ * @argc: number of arguments
+ * @argv: arguments
+ * Return: return the result of operation
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	int (*func)(int, int);
+	int a, b, result;
+
+	func = get_op_func(argv[2]);
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	if (!func())
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if ((argv[2] == "/" || argv[2] == "%") && argv[3] == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	result = func(a, b);
+	printf("%d\n", result);
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "calc.h"
+/**
+ * op_add - add 2 numbers
+ * @a: first number
+ * @b: second number
+ * Return: return sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - substract 2 numbers
+ * @a: first number
+ * @b: second number
+ * Return: return sum
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiply 2 numbers
+ * @a: first number
+ * @b: second number
+ * Return: return mul
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divide 2 numbers
+ * @a: first number
+ * @b: second number
+ * Return: return division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - calculate the modulo
+ * @a: first number
+ * @b: second number
+ * Return: return modulo
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
