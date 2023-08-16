681b9767ab9568cb0a79f2ac04fe2f5a4ee0d3a8
ilyas <ilyasouagag2011@gmail.com>
2023-08-16 17:50:57 +0100

    test

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -14,14 +14,13 @@ int (*get_op_func(char *s))(int, int)
 		{"/", op_div},
 		{"%", op_mod},
 		{NULL, NULL}};
-	int i = 0;
+	int i;
 
+	i = 0;
 	while (ops[i].op)
 	{
 		if (strcmp(ops[i].op, s) == 0)
-		{
 			return (ops[i].f);
-		}
 		i++;
 	}
 	return (0);
