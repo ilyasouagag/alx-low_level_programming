0df040ad56cf8682541d7daaa909889c448fda4a
ilyas <ilyasouagag2011@gmail.com>
2023-08-16 17:42:18 +0100

    it's gonna work

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -18,12 +18,12 @@ int main(int argc, char *argv[])
 	func = get_op_func(argv[2]);
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
-	if (!func())
+	if (!func)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if ((argv[2] == '/' || argv[2] == '%') && argv[3] == 0)
+	if ((argv[2] == "/" || argv[2] == "%") && argv[3] == 0)
 	{
 		printf("Error\n");
 		exit(100);
