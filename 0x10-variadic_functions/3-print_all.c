f65e50ca5d6c53a3815002ff90805b9d5b04d843
ilyas <ilyasouagag2011@gmail.com>
2023-08-17 20:15:16 +0100

    prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -18,13 +18,13 @@ void print_all(const char *const format, ...)
 			switch (format[i])
 			{
 			case 'c':
-				printf("%s%c", separator, va_arg(args, char));
+				printf("%s%c", separator, va_arg(args, int));
 				break;
 			case 'i':
 				printf("%s%d", separator, va_arg(args, int));
 				break;
 			case 'f':
-				printf("%s%f", separator, va_arg(args, float));
+				printf("%s%f", separator, va_arg(args, double));
 				break;
 			case 's':
 				s = va_arg(args, char *);
@@ -42,4 +42,9 @@ void print_all(const char *const format, ...)
 	}
 	printf("\n");
 	va_end(args);
+}
+int main(void)
+{
+    print_all("ceis", 'B', 3, "stSchool");
+    return (0);
 }
