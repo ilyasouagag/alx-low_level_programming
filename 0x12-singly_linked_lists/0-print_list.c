9c9a79096d74c77f9751b0501c8bdb0997b31757
ilyas <ilyasouagag2011@gmail.com>
2023-08-24 13:44:44 +0100

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <string.h>
 /**
  * print_list - function that prints all the elements
  * @h: pointer to struct
@@ -16,8 +17,38 @@ size_t print_list(const list_t *h)
 			printf("[0] (nil)");
 			continue;
 		}
-		printf("[%d] %s\n", current->len, current->str);
+		printf("[%u] %s\n", current->len, current->str);
 		count++;
 	}
 	return (count);
+}
+int main(void)
+{
+    list_t *head;
+    list_t *new;
+    list_t hello = {"World", 5, NULL};
+    size_t n;
+
+    head = &hello;
+    new = malloc(sizeof(list_t));
+    if (new == NULL)
+    {
+        printf("Error\n");
+        return (1);
+    }
+    new->str = strdup("Hello");
+    new->len = 5;
+    new->next = head;
+    head = new;
+    n = print_list(head);
+    printf("-> %lu elements\n", n);
+
+    printf("\n");
+    free(new->str);
+    new->str = NULL;
+    n = print_list(head);
+    printf("-> %lu elements\n", n);
+
+    free(new);
+    return (0);
 }
