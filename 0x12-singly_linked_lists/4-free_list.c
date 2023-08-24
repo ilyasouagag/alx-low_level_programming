e17087f8d58203d3e1d037032da58ada611e1846
ilyas <ilyasouagag2011@gmail.com>
2023-08-24 15:17:17 +0100

    free linked lists

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -5,13 +5,13 @@
  */
 void free_list(list_t *head)
 {
+	list_t *current;
+
 	while (head)
 	{
-		list_t *current = head;
-
-		while (current->next != NULL)
-			current = current->next;
-
-		free(current);
+		current = head->next;
+		free(head->str);
+		free(head);
+		head = current;
 	}
 }
