593408c075647039a82113549a65cdac42feafb1
ilyas <ilyasouagag2011@gmail.com>
2023-08-28 18:03:59 +0100

    free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,12 +6,15 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *current;
+	listint_t *temp = *head;
 
-	while (*head)
+	if (head == NULL)
+		return;
+	while (temp)
 	{
-		current = (*head)->next;
-		free(*head);
-		*head = current;
+		current = temp->next;
+		free(temp);
+		temp = current;
 	}
 	*head = NULL;
 }
