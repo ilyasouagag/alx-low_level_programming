8f3b8c2aef468f512a1e37215ffaf6e60781b8f0
ilyas <ilyasouagag2011@gmail.com>
2023-08-29 17:38:01 +0100

    free safe version

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+/**
+ * free_listint_safe - frees a linked list
+ * @h: pointer to node
+ * Return: number of elements
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t count = 0;
+	listint_t *current, *temp;
+
+	if (!h || !*h)
+		return (0);
+	current = *h;
+	while (current != NULL)
+	{
+		temp = current->next;
+
+		if ((int)current > (int)(current->next))
+		{
+			free(current);
+			current = temp;
+			count++;
+		}
+		else
+		{
+			free(current);
+			current = NULL;
+			count++;
+			break;
+		}
+	}
+	*h = NULL;
+	return (count);
+}
