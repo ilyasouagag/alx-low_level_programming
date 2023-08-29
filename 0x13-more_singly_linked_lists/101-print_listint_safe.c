eae1ce9a5074fce652c395a9ebd31cb9e7b840e4
ilyas <ilyasouagag2011@gmail.com>
2023-08-29 17:01:31 +0100

    print safe version

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+/**
+ * print_listint_safe - function that prints linked list
+ * @head: pointer to struct
+ * Return: return count
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *current;
+	const listint_t *prev;
+	size_t count = 0;
+	size_t a;
+
+	current = prev = NULL;
+	current = head;
+	if (head == NULL)
+		exit(98);
+	while (current != NULL)
+	{
+		printf("[%p] %d\n", (void *)current, current->n);
+		count++;
+		current = current->next;
+		prev = head;
+		a = 0;
+		while (a < count)
+		{
+			if (current == prev)
+			{
+				printf("-> [%p] %d\n", (void *)current, current->n);
+				return (count);
+			}
+			prev = prev->next;
+			a++;
+		}
+	}
+	return (count);
+}
