7a2a97d264c40629f4db1c8f5dea9fd18a34fa80
ilyas <ilyasouagag2011@gmail.com>
2023-08-31 15:32:25 +0100

    decimal to binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * print_binary - decimal to binary
+ * @n: log decimal
+ */
+void print_binary(unsigned long int n)
+{
+	int i;
+	int v = 0;
+
+	for (i = 31; i >= 0; i--)
+	{
+		v = n >> i & 1;
+		if (v == 1)
+		{
+			while (i >= 0)
+			{
+				putchar((n >> i & 1) + '0');
+				i--;
+			}
+		}
+	}
+}
+int main(void)
+{
+	print_binary(0);
+	printf("\n");
+	print_binary(1);
+	printf("\n");
+	print_binary(98);
+	printf("\n");
+	print_binary(1024);
+	printf("\n");
+	print_binary((1 << 10) + 1);
+	printf("\n");
+	return (0);
+}
