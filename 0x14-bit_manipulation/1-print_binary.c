e6c6be7b861e7e84219f96e0a65889ae6c225b94
ilyas <ilyasouagag2011@gmail.com>
2023-08-31 16:32:24 +0100

    decimal to binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,7 +7,11 @@ void print_binary(unsigned long int n)
 {
 	int i;
 	int v = 0;
-
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
 	for (i = 31; i >= 0; i--)
 	{
 		v = n >> i & 1;
@@ -15,9 +19,23 @@ void print_binary(unsigned long int n)
 		{
 			while (i >= 0)
 			{
-				putchar((n >> i & 1) + '0');
+				_putchar((n >> i & 1) + '0');
 				i--;
 			}
 		}
 	}
+}
+int main(void)
+{
+	print_binary(0);
+	printf("\n");
+	print_binary(1);
+	printf("\n");
+	print_binary(98);
+	printf("\n");
+	print_binary(1024);
+	printf("\n");
+	print_binary((1 << 10) + 1);
+	printf("\n");
+	return (0);
 }
