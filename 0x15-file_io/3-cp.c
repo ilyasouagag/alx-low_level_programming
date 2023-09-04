e9104ae19e4f4e79a950b92e408fb6c993bd3228
ilyas <ilyasouagag2011@gmail.com>
2023-09-04 19:03:34 +0100

    copie from a file to another

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,14 +7,12 @@
  */
 int main(int argc, char **argv)
 {
-	int fp, f, p;
+	int fp, f;
+	ssize_t p, pf;
 	char buffer[1024];
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	fp = open(argv[1], O_RDONLY);
 	if (fp == -1)
 	{
@@ -23,26 +21,23 @@ int main(int argc, char **argv)
 	}
 	f = open(argv[2], O_RDWR | O_TRUNC | O_CREAT, 0644);
 	if (f == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]), exit(99);
+	while ((p = read(fp, buffer, sizeof(buffer))) > 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]);
-		exit(99);
+		pf = write(f, buffer, p);
+		if (pf == -1)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]), exit(99);
 	}
-	while ((p = read(fp, buffer, 1024)) > 0)
-		if ((write(f, buffer, p)) != p)
-			dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]);
 	if (p == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 	fp = close(fp);
 	f = close(f);
 	if (fp)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fp);
-		exit(100);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fp), exit(100);
 	if (f)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f);
-		exit(100);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f), exit(100);
 	return (0);
 }
