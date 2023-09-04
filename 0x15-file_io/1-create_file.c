46e363058effc372555c52036d1d0460ec8abe5f
ilyas <ilyasouagag2011@gmail.com>
2023-09-04 17:33:28 +0100

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * create_file - function that creates a file
+ * @filename: the file created
+ * @text_content: content to append into the file
+ * Return: return 1 on success else -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int p;
+	int i = 0, len = 0;
+	int fp;
+
+	if (text_content == NULL)
+		return (0);
+	if (filename == NULL)
+		return (-1);
+	while (text_content[i])
+	{
+		len++;
+		i++;
+	}
+	p = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (p == -1)
+		return (-1);
+	fp = write(p, text_content, len);
+	close(p);
+	if (fp == -1)
+		return (-1);
+	return (1);
+}
