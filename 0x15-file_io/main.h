2652b4ca1c5cabe3053b13e76e1aeb05c10f54f8
ilyas <ilyasouagag2011@gmail.com>
2023-09-04 20:05:30 +0100

    bzf

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,8 +4,10 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <sys/types.h>
 #include <sys/stat.h>
 #include <errno.h>
+#include <elf.h>
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 void exit_with_error(int code, const char *message, const char *filename);
