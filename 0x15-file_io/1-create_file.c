043ba68528adc60fe89b98f2fd55b865ade8ae32
ilyas <ilyasouagag2011@gmail.com>
2023-09-04 17:47:06 +0100

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,10 +12,10 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	while (text_content[len] && text_content != NULL)
-	{
-		len++;
-	}
+	if (text_content)
+		while (text_content[len])
+			len++;
+
 	p = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (p == -1)
 		return (-1);
