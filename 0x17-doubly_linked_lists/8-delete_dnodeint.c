b00c2d34002a22f565fc2768eb63352176fd593e
ilyas <ilyasouagag2011@gmail.com>
2023-10-12 19:49:32 +0100

    delete at pos

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -10,7 +10,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *current;
 	unsigned int i;
 
-	if (!head)
+	if (!head || !(*head))
 		return (-1);
 	if (index == 0)
 	{
