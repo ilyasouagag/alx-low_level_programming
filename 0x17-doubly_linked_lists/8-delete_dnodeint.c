9b402e09ba7ae69f69d3743baff80872d59152a7
ilyas <ilyasouagag2011@gmail.com>
2023-10-12 19:30:54 +0100

    delete at pos

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index - delete node at index
+ * @head: head of list
+ * @index: index to delete
+ * Return: return 1 if succes, else 0
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *current;
+	unsigned int i;
+
+	if (!(*head) || !head)
+		return (-1);
+	if (index == 0)
+	{
+		current = (*head);
+		*(head) = current->next;
+		free(current);
+		return (1);
+	}
+	for (i = 0, current = *head; i < index; i++)
+	{
+		current = current->next;
+	}
+	current->prev->next = current->next;
+	current->next->prev = current->prev;
+	free(current);
+	return (1);
+}
