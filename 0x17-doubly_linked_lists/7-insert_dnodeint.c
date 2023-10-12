fca7d18317be8964b04e312f68dc7af778f61edd
ilyas <ilyasouagag2011@gmail.com>
2023-10-12 17:28:41 +0100

    insert at position

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -18,7 +18,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		count++;
 	}
-	if (idx >= count)
+	if (idx > count)
 		return (NULL);
 	temp = malloc(sizeof(dlistint_t));
 	if (!temp)
@@ -30,7 +30,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		return (add_dnodeint(h, n));
 	}
-	if (idx == count - 1)
+	if (idx == count)
 	{
 		return (add_dnodeint_end(h, n));
 	}
