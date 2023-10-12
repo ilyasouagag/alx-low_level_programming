3c63267a5a222c745313f0245b6de3e818cd4a9d
ilyas <ilyasouagag2011@gmail.com>
2023-10-12 18:40:28 +0100

    insert at pos

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -26,23 +26,26 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	if (idx == 0)
 	{
-		return (add_dnodeint(h, n));
+		temp = add_dnodeint(h, n);
 	}
-	if (idx == count)
+	else if (idx == count)
 	{
-		return (add_dnodeint_end(h, n));
+		temp = add_dnodeint_end(h, n);
 	}
-	temp = malloc(sizeof(dlistint_t));
-	if (!temp)
-		return (NULL);
-	for (i = 0, node = other; i < idx; i++)
+	else
 	{
-		node = node->next;
+		temp = malloc(sizeof(dlistint_t));
+		if (!temp)
+			return (NULL);
+		for (i = 0, node = other; i < idx; i++)
+		{
+			node = node->next;
+		}
+		temp->n = n;
+		node->prev->next = temp;
+		temp->prev = node->prev;
+		temp->next = node;
+		node->prev = temp;
 	}
-	temp->n = n;
-	node->prev->next = temp;
-	temp->prev = node->prev;
-	temp->next = node;
-	node->prev = temp;
 	return (temp);
 }
