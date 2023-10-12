eba59e1e28b4079543d3fdbc1bb6abd9df8d8150
ilyas <ilyasouagag2011@gmail.com>
2023-10-12 18:43:18 +0100

    insert at pos

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -22,8 +22,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		count++;
 	}
-	if (idx > count)
-		return (NULL);
 	if (idx == 0)
 	{
 		temp = add_dnodeint(h, n);
