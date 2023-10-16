7de6207676dcc4d890dc103e0dd5389318ec1590
ilyas <ilyasouagag2011@gmail.com>
2023-10-16 19:57:10 +0100

    advanced task

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/ilyasouagag/alx-low_level_programming/blob/4362e515f66afe3fd00dd0116892c0f29fcad07c/0x18-dynamic_libraries/library_advanced.so
-export LD_PRELOAD=/tmp/library_advanced.so
+wget -P /tmp https://github.com/ilyasouagag/alx-low_level_programming/raw/master/0x18-dynamic_libraries/lib_adv.so
+export LD_PRELOAD=/tmp/lib_adv.so
