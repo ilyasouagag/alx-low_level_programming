872aea6fd49eaa0f031bc9e5f6122fe6757d34cf
ilyasouagag <ilyasouagag2011@gmail.com>
2023-12-01 01:27:31 +0100

    mandatory tasks

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,6 +1,9 @@
 #include "hash_tables.h"
 /**
- * 
+ * key_index - function that gives the index of a key
+ * @key: key of the node
+ * @size: size of table
+ * Return: return the index
 */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,11 +1,15 @@
 #include "hash_tables.h"
 /**
- * 
-*/
+ * hash_table_set - function that adds ana elemtn to the hash table
+ * @ht: pointer to table
+ * @key: key of the node
+ * @value: value of the node
+ * Return: return if the element has been added or no
+ */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t* current;
-	hash_node_t* another;
+	hash_node_t *current;
+	hash_node_t *another;
 	unsigned long int index;
 
 	current = malloc(sizeof(hash_node_t));
@@ -13,9 +17,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 
 	current->key = malloc(strlen(key) + 1);
-    current->value = malloc(strlen(value) + 1);
-    strcpy(current->key, key);
-    strcpy(current->value, value);
+	current->value = malloc(strlen(value) + 1);
+	strcpy(current->key, key);
+	strcpy(current->value, value);
 	current->next = NULL;
 
 	index = key_index((const unsigned char *)key, ht->size);
