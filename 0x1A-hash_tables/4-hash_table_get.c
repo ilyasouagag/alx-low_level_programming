e7a09fae3fb9c9cdaefc86c005b81a68a9adc73c
ilyasouagag <ilyasouagag2011@gmail.com>
2023-12-01 01:54:37 +0100

    get hash table

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -7,20 +7,20 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-    unsigned long int index;
-    hash_node_t *current;
+	unsigned long int index;
+	hash_node_t *current;
 
-    if (ht == NULL || key == NULL || *key == '\0')
+	if (ht == NULL || key == NULL || *key == '\0')
 		return (NULL);
-    index = key_index((const unsigned char *)key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 
-    current = ht->array[index];
+	current = ht->array[index];
 
-    while (current && strcmp(current->key, key) != 0)
-    {
-        current = current->next;
-    }
-    if (current == NULL)
-        return (NULL);
-    return (current->value);
+	while (current && strcmp(current->key, key) != 0)
+	{
+		current = current->next;
+	}
+	if (current == NULL)
+		return (NULL);
+	return (current->value);
 }
