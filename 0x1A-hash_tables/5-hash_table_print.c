0d7b2c02e2811a47e7f1f59e5feb49fe511820ab
ilyasouagag <ilyasouagag2011@gmail.com>
2023-12-01 02:17:44 +0100

    print a hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+/**
+ *
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *current;
+	int flag = 0;
+
+	if (ht == NULL)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			if (flag == 1)
+				printf(", ");
+			current = ht->array[i];
+			while (current)
+			{
+				printf("'%s' : '%s'", current->key, current->value);
+				current = current->next;
+				if (current)
+					printf(", ");
+			}
+			flag = 1;
+		}
+	}
+	printf("}\n");
+}
