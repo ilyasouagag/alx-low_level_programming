5657775769e0b77880f7abf20bbf5b4927f9caa9
ilyasouagag <ilyasouagag2011@gmail.com>
2023-12-01 02:24:27 +0100

    print a table

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -4,28 +4,30 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
+	hash_node_t *node;
 	unsigned long int i;
-	hash_node_t *current;
-	int flag = 0;
+	unsigned char comma_flag = 0;
 
 	if (ht == NULL)
 		return;
+
 	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
 		if (ht->array[i] != NULL)
 		{
-			if (flag == 1)
+			if (comma_flag == 1)
 				printf(", ");
-			current = ht->array[i];
-			while (current)
+
+			node = ht->array[i];
+			while (node != NULL)
 			{
-				printf("'%s' : '%s'", current->key, current->value);
-				current = current->next;
-				if (current)
+				printf("'%s': '%s'", node->key, node->value);
+				node = node->next;
+				if (node != NULL)
 					printf(", ");
 			}
-			flag = 1;
+			comma_flag = 1;
 		}
 	}
 	printf("}\n");
