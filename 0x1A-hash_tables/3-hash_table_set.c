004fdfa967d5492ec1f2b59f14a02f45aafe5218
ilyasouagag <ilyasouagag2011@gmail.com>
2023-12-01 03:25:35 +0100

    set a hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,25 +9,29 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *current;
+	hash_node_t *temp;
 	unsigned long int index;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
-	current = malloc(sizeof(hash_node_t));
-	if (!current)
-		return (0);
-
-	current->key = malloc(strlen(key) + 1);
-	if (!current->key)
-		return (0);
-	current->value = malloc(strlen(value) + 1);
-	if (!current->value)
-		return (0);
-	strcpy(current->key, key);
-	strcpy(current->value, value);
-	current->next = NULL;
 
 	index = key_index((const unsigned char *)key, ht->size);
+	temp = ht->array[index];
+	while (temp != NULL)
+	{
+		if (strcmp(temp->key, key) == 0)
+		{
+			free(temp->value);
+			temp->value = strdup(value);
+			if (!temp->value)
+				return (0);
+			return (1);
+		}
+		temp = temp->next;
+	}
+	current = create(key, value);
+	if (!current)
+		return (0);
 	if (ht->array[index])
 	{
 		current->next = ht->array[index];
@@ -38,4 +42,29 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		ht->array[index] = current;
 	}
 	return (1);
+}
+/**
+ * create - create a node with key and value
+ * @key: key of node
+ * @value: value of node
+ * Return: return the node
+*/
+hash_node_t *create(const char *key, const char *value)
+{
+	hash_node_t *current;
+
+	current = malloc(sizeof(hash_node_t));
+	if (!current)
+		return (NULL);
+
+	current->key = malloc(strlen(key) + 1);
+	if (!current->key)
+		return (NULL);
+	current->value = malloc(strlen(value) + 1);
+	if (!current->value)
+		return (NULL);
+	strcpy(current->key, key);
+	strcpy(current->value, value);
+	current->next = NULL;
+	return (current);
 }
